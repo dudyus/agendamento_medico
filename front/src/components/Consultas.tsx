2ba35c3d1698ebed543314ce196fd461e857c3b6
Vítor manoel mascarenhas <vitormanoelmb02@gmail.com>
2025-09-25 21:30:04 -0300

    Minhas Consultas Mostrando por ID

--- front/src/components/Consultas.tsx
+++ front/src/components/Consultas.tsx
@@ -1,22 +1,29 @@
 // src/pages/Consultas.tsx
 import { useEffect, useState } from "react";
+import { usePacienteStore } from "../context/PacienteContext"; 
+import { useNavigate } from "react-router-dom";
 
 type Consulta = {
   id: number;
   data: string;
   hora: string;
-  paciente: { nome: string };
+  paciente: { id: number; nome: string };
   profissional: { nome: string };
 };
 
 export default function Consultas() {
   const [consultas, setConsultas] = useState<Consulta[]>([]);
+  const { paciente } = usePacienteStore(); // pega o paciente logado
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/consultas`)
       .then((res) => res.json())
-      .then(setConsultas);
-  }, []);
+      .then((data: Consulta[]) => {
+        const minhas = data.filter((c) => String(c.paciente.id) === String(paciente.id));
+        setConsultas(minhas);
+      });
+  }, [paciente, navigate]);
 
   return (
     <div className="p-4">
@@ -24,7 +31,7 @@ export default function Consultas() {
       <ul>
         {consultas.map((c) => (
           <li key={c.id} className="mb-2 border p-2 rounded">
-            {c.data} {c.hora} - {c.paciente.nome} com {c.profissional.nome}
+            {c.data} {c.hora} - {c.profissional.nome}
           </li>
         ))}
       </ul>
