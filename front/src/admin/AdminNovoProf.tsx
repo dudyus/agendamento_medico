f6659cd736ccf832da130e5fb3f933d4aeec8841
VITOR MANOEL MASCARENHAS BORBA <04115952024@senacrs.edu.br>
2025-10-10 10:50:58 -0300

    Cad Profissional funcionando

--- front/src/admin/AdminNovoProf.tsx
+++ front/src/admin/AdminNovoProf.tsx
@@ -13,6 +13,7 @@ type Inputs = {
   ano_inicio_carreira: number
   foto: string 
   id_funcao: number
+  destaque: boolean
 }
 
 export default function AdminNovoProf() {
@@ -48,7 +49,8 @@ export default function AdminNovoProf() {
         genero: data.genero,
         ano_inicio_carreira: Number(data.ano_inicio_carreira),
         foto: data.foto,
-        id_funcao: Number(data.id_funcao)
+        id_funcao: Number(data.id_funcao),
+        destaque: data.destaque
     }
 
     const response = await fetch(`${apiUrl}/profissionais`,
@@ -115,6 +117,17 @@ export default function AdminNovoProf() {
               {...register("idade")}
             />
           </div>
+          <div className="mb-3">
+            <label htmlFor="destaque" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Profissional em Destaque?
+            </label>
+            <input
+              type="checkbox"
+              id="destaque"
+              {...register("destaque")}
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+          </div>
         </div>
         <div className="grid gap-6 mb-3 md:grid-cols-2">
           <div className="mb-3">
@@ -136,8 +149,8 @@ export default function AdminNovoProf() {
               <option>FEMININO</option>
             </select>
           </div>
+          
         </div>
-
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Incluir</button>
       </form>
