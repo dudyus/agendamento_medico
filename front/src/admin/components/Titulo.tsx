05fd788cce2f481f000e68cccc72d6c4e468a0ad
Vítor manoel mascarenhas <vitormanoelmb02@gmail.com>
2025-10-11 14:11:43 -0300

    NavBar adm arrumada

--- front/src/admin/components/Titulo.tsx
+++ front/src/admin/components/Titulo.tsx
@@ -74,7 +74,7 @@ export default function Titulo() {
                 Sair
               </button>
             </div>
-          ) : (
+          ) : admin?.nome ? null : (
             <Link
               to="/login"
               className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center transition"
