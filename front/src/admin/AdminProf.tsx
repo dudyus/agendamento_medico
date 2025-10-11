f65fba5b39d8b9500516c623fee9a6ffd6ee5642
unknown <eduardolealsoares2005@gmail.com>
2025-10-11 12:47:13 -0300

    Botões e funcionamento da aerea restrita

--- front/src/admin/AdminProf.tsx
+++ front/src/admin/AdminProf.tsx
@@ -28,7 +28,7 @@ export default function AdminProf() {
           Cadastro de profissionais
         </h1>
         <Link to="/admin/profissionais/novo" 
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 text-white font-medium rounded px-4 py-1.5 text-sm mb-4">
           Novo profissional
         </Link>
       </div>
--- front/src/admin/components/MenuLateral.tsx
+++ front/src/admin/components/MenuLateral.tsx
@@ -1,9 +1,7 @@
 import { useAdminStore } from "../context/AdminContext"
-import { IoExitOutline } from "react-icons/io5"
+import { IoExit } from "react-icons/io5";
 import { BiSolidDashboard } from "react-icons/bi"
-import { FaCarSide, FaUsers } from "react-icons/fa6"
-import { BsCashCoin } from "react-icons/bs"
-
+import { FaIdCard, FaMedrt, FaUserPlus  } from "react-icons/fa6"
 import { Link, useNavigate } from "react-router-dom"
 
 export function MenuLateral() {
@@ -20,51 +18,51 @@ export function MenuLateral() {
   return (
 <aside
   id="default-sidebar"
-  className="w-64 h-auto bg-blue-300 dark:bg-gray-800"
+  className="w-64 h-auto bg-blue-9  00 dark:bg-gray-800"
   aria-label="Sidebar"
->      <div className="h-full px-3 py-4 bg-blue-300 dark:bg-gray-800 ">
+>      <div className="h-full px-3 py-4 bg-blue-900 dark:bg-gray-800 ">
         <ul className="space-y-2 font-medium">
         <li>
             <Link to="/admin" className="flex items-center p-2">
-              <span className="h-5 text-gray-600 text-2xl">
+              <span className="h-5 text-white text-2xl">
                 <BiSolidDashboard />
               </span>
-              <span className="ms-2 mt-1">Visão Geral</span>
+              <span className="ms-2 mt-1 text-white">Visão Geral</span>
             </Link>
           </li>
           <li>
             <Link to="/admin/cadastro-profissionais" className="flex items-center p-2">
-              <span className="h-5 text-gray-600 text-2xl">
-                <FaCarSide />
+              <span className="h-5 text-white text-2xl">
+                <FaIdCard  />
               </span>
-              <span className="ms-2 mt-1">Cadastro de Profissionais</span>
+              <span className="ms-2 mt-1 text-white">Cadastro de Profissionais</span>
             </Link>
           </li>
           <li>
           <Link to="/admin/cadastro-pacientes" className="flex items-center p-2">
-              <span className="h-5 text-gray-600 text-2xl">
-                <FaUsers />
+              <span className="h-5 text-white text-2xl">
+                <FaUserPlus />
               </span>
-              <span className="ms-2 mt-1">Controle de Pacientes</span>
+              <span className="ms-2 mt-1 text-white">Controle de Pacientes</span>
             </Link>
           </li>
           <li>
           <Link to="/admin/controle-consultas" className="flex items-center p-2 cursor-pointer">
-              <span className="h-5 text-gray-600 text-2xl">
-                <BsCashCoin />
+              <span className="h-5 text-white text-2xl">
+                <FaMedrt  />
               </span>
-              <span className="ms-2 mt-1">Controle de Consultas</span>
+              <span className="ms-2 mt-1 text-white">Controle de Consultas</span>
+            </Link>
+          </li>
+          <li>
+          <Link to="/admin/" className="flex items-center p-2 cursor-pointer">
+              <span className="h-5 text-white text-2xl">
+                <IoExit  />
+              </span>
+              <span className="ms-2 mt-1 text-white" onClick={adminSair}>Sair do Sistema</span>
             </Link>
           </li>
 
-          <li>
-            <span className="flex items-center p-2 cursor-pointer">
-              <span className="h-5 text-gray-600 text-2xl">
-                <IoExitOutline />
-              </span>
-              <span className="ms-2 mt-1" onClick={adminSair}>Sair do Sistema</span>
-            </span>
-          </li>
         </ul>
       </div>
     </aside>
